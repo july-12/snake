edbf1752aaf2a2d1fde7caa72f38cc6d685d07c3
tanyb <seventanyb@gmail.com>
2018-05-21 11:34:19 +0800

    amend readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-## 贪吃蛇小游戏
+# 贪吃蛇小游戏
 
-### tech
+## Tech
 
     react + redux + svg
+
+![alt text](/snake.png)
+
+## Run Project
+
+    1. npm install
+
+    2. npm start
--- src/reducers/meat.js
+++ src/reducers/meat.js
@@ -4,7 +4,7 @@ let _ = lodash
 
 let initialState = {
   x: _.random(Math.floor(30)) * 10,
-  y: _.random(Math.floor(30)) * 10
+  y: _.random(Math.floor(40)) * 10
 }
 export default (state = initialState, action) => {
   switch (action.type) {
